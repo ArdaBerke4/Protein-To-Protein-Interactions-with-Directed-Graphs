ee7199da7644b2819306d29933819a884e4bf2f5
ArdaBerke4 <ardaberke221@gmail.com>
2025-12-05 18:30:54 +0300

    initial commit

---
+++ src/HW2/Edge.java
@@ -0,0 +1,31 @@
+package HW2;
+
+public class Edge {
+    private String source;
+    private String target;
+    private double score;
+
+
+    public Edge(String source, String target, double score) {
+        this.source = source;
+        this.target = target;
+        this.score = score;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return source + " -> " + target + " [" + score + "]";
+    }
+}
